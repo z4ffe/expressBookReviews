e5db2eb959463fa68292cbcb6367562f5b140f94
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-05-05 00:56:28 +0300

    some error handling and books controller

--- utils/userValidate.ts
+++ utils/userValidate.ts
@@ -1,11 +1,8 @@
 import {users} from '../entities/users'
 
-type UserValidate = {
-	userExist: boolean
-	userValidation: boolean
-}
+type UserValidate = (name: string, password: string) => {userExist: boolean, userValidation: boolean}
 
-export const userValidate = (name: string, password: string): UserValidate => {
+export const userValidate: UserValidate = (name, password) => {
 	const userExist = !!users.filter(user => user.name === name)[0]
 	const userValidation = !name || !password || password.length < 4 || name.length < 2
 	return {userExist, userValidation}
